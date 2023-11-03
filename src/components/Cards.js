21dc08a6cd864a6a06736821b9cad76f82360929
saluumaa <salumahassan02@gmail.com>
2023-11-03 15:13:18 +0300

    Create react app

---
+++ src/components/Cards.js
@@ -0,0 +1,30 @@
+// import BgCardFront from '../images/bg-card-front.png';
+
+// const Cards = () => {
+//     return (
+//     <div class="cards">
+//         <div class="card">
+//         <div class="card-front">
+//         <img src={BgCardFront} alt="card-front" class="card-front-image" />
+//         <div class="card-front-info">     
+//             <p class="card__front--info__number">1234 5678 9012 3456</p>
+//             <div className='card-info'>
+//                 <h2 class="card__front--info__name">Salma Hassan</h2>
+//                 <p class="card__front--info__expiry">12/21</p>
+//             </div>
+           
+//         </div>
+//         </div>
+//         {/* <div class="card-back">
+//         <img src={BgCardBack} alt="card-back" class="card-back-image" />
+//         <div class="card-back-info">
+//             <div class="card-back-info-cvv">123</div>
+//         </div> */}
+//         {/* </div> */}
+//         </div>
+//     </div>
+
+//     )
+// }
+
+// export default Cards;   
